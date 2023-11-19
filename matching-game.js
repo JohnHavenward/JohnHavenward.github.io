b1fef416869e270e238654941dcc9a245e7ce3ba
JohnHavenward <131699940+JohnHavenward@users.noreply.github.com>
2023-11-19 21:28:42 +0100

    Add files via upload

--- matching-game.js
+++ matching-game.js
@@ -484,3 +484,32 @@ function TimerStart() {
 	}
 
 }
+
+
+
+
+function watchForHover() {
+
+      let lastTouchTime = 0
+    
+      function enableHover() {
+        if (new Date() - lastTouchTime < 500) return
+        document.body.classList.add('hasHover')
+      }
+    
+      function disableHover() {
+        document.body.classList.remove('hasHover')
+      }
+    
+      function updateLastTouchTime() {
+        lastTouchTime = new Date()
+      }
+    
+      document.addEventListener('touchstart', updateLastTouchTime, true)
+      document.addEventListener('touchstart', disableHover, true)
+      document.addEventListener('mousemove', enableHover, true)
+    
+      enableHover()
+    }
+    
+    watchForHover()
