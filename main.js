9a7be6700541e604fbc6dd95510501e330eea96c
John <john.havenward@gmail.com>
2024-10-14 20:44:54 +0200

    artmustgoon.com page load

--- main.js
+++
@@ -1,7 +0,0 @@
-let xmlhttp;
-  
-xmlhttp= new XMLHttpRequest();
-xmlhttp.open('GET', "README.md", false);
-xmlhttp.send();
-
-document.getElementById('content').innerHTML = marked.parse(xmlhttp.responseText);
